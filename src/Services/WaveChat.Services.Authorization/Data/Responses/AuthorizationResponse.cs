1fea72e771def67d3d793f1f06cc8c19f14558e2
Kovshik387 <fantokin03@mail.ru>
2024-03-04 19:49:44 +0300

    feat: add authorization service

---
+++ src/Services/WaveChat.Services.Authorization/Data/Responses/AuthorizationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveChat.Services.Authorization.Data.Responses
+{
+    public class AuthorizationResponse <T>
+    {
+    }
+}
---
+++ src/Services/WaveChat.Services.Authorization/Infastructure/IAuthorizationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveChat.Services.Authorization.Data.DTO;
+using WaveChat.Services.Authorization.Data.Responses;
+
+namespace WaveChat.Services.Authorization.Infastructure
+{
+    /// <summary>
+    /// Сервис бизнес-логики для авторизации и регистрации
+    /// </summary>
+    public interface IAuthorizationService
+    {
+        /// <summary>
+        /// Аутентификация в существующий аккаунт
+        /// </summary>
+        /// <param name="signIn"></param>
+        /// <returns></returns>
+        Task<AuthorizationResponse<AuthorizationDTO>> SignInAsync(SignInDTO signIn);
+        
+    }
+}
