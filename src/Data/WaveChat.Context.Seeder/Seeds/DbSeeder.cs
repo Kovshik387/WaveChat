c96ca52a120a87d21af57eb6828b3aa72ea0aabd
Kovshik387 <fantokin03@mail.ru>
2024-03-04 19:49:12 +0300

    feat: add demo data

---
+++ src/Data/WaveChat.Context.Seeder/Seeds/DbSeeder.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveChat.Context.Seeder.Seeds.Demo;
+using WaveChat.Context.Settings;
+
+namespace WaveChat.Context.Seeder.Seeds;
+
+public static class DbSeeder
+{
+    private static IServiceScope ServiceScope(IServiceProvider serviceProvider)
+    {
+        return serviceProvider.GetService<IServiceScopeFactory>()!.CreateScope();
+    }
+
+    private static CorporateMessengerContext DbContext(IServiceProvider serviceProvider)
+    {
+        return ServiceScope(serviceProvider)
+            .ServiceProvider.GetRequiredService<IDbContextFactory<CorporateMessengerContext>>().CreateDbContext();
+    }
+
+    public static void Execute(IServiceProvider serviceProvider)
+    {
+        Task.Run(async () =>
+        {
+            await AddDemoData(serviceProvider);
+            //await AddAdministrator(serviceProvider);
+        })
+            .GetAwaiter()
+            .GetResult();
+    }
+
+    private static async Task AddDemoData(IServiceProvider serviceProvider)
+    {
+        using var scope = ServiceScope(serviceProvider);
+        if (scope == null)
+            return;
+
+        //var settings = scope.ServiceProvider.GetService<DbSettings>();
+        ////if (!(settings.Init?.AddDemoData ?? false))
+        ////    return;
+
+        await using var context = DbContext(serviceProvider);
+
+        if (await context.Rolestypes.AnyAsync())
+            return;
+
+        await context.Rolestypes.AddRangeAsync(new DemoHelper().GetRoles);
+
+        await context.SaveChangesAsync();
+    }
+
+    private static async Task AddAdministrator(IServiceProvider serviceProvider)
+    {
+        //using var scope = ServiceScope(serviceProvider);
+        //if (scope == null)
+        //    return;
+
+        //var settings = scope.ServiceProvider.GetService<DbSettings>();
+        //if (!(settings.Init?.AddAdministrator ?? false))
+        //    return;
+
+        //var userAccountService = scope.ServiceProvider.GetService<IUserAccountService>();
+
+        //if (!(await userAccountService.IsEmpty()))
+        //    return;
+
+        //await userAccountService.Create(new RegisterUserAccountModel()
+        //{
+        //    Name = settings.Init.Administrator.Name,
+        //    Email = settings.Init.Administrator.Email,
+        //    Password = settings.Init.Administrator.Password,
+        //});
+    }
+}
---
+++ src/Data/WaveChat.Context.Seeder/Seeds/Demo/DemoHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveChat.Context.Entities.Users;
+
+namespace WaveChat.Context.Seeder.Seeds.Demo;
+
+public class DemoHelper
+{
+    public IEnumerable<Rolestype> GetRoles = new List<Rolestype>
+    {
+        new Rolestype()
+        {
+            Idroletype = 1,
+            Rolename = "Работник"
+        },
+        new Rolestype()
+        {
+            Idroletype = 2,
+            Rolename = "Старший программист"
+        }
+    };
+}
